2c5fc91fdabf5faaf68a3e793f36c29d4edd0e8e
adibrov <dibrale@gmail.com>
2025-08-31 20:13:23 -0500

    Set loglevel to debug for empty queue spam

--- modules/utils.py
+++ modules/utils.py
@@ -149,12 +149,12 @@ async def until_empty(queue: Queue, interval: float = 0.1, timeout: float = 3) -
         timeout (float, optional): The maximum time to wait in seconds. Defaults to 3.
     """
     if queue.empty():
-        logging.info("Queue is already empty")
+        logging.debug("Queue is already empty")
         return True
     start_time = asyncio.get_event_loop().time()
     while asyncio.get_event_loop().time() - start_time < timeout:
         if queue.empty():
-            logging.info(f"Queue {str(queue)} is now empty")
+            logging.debug(f"Queue {str(queue)} is now empty")
             return True
         else:
             await asyncio.sleep(interval)
